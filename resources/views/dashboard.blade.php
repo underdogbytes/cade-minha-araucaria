32d15dc2bc0bee0445fc0e471cca593e6dcb2f54
bea <beatriztattooer@gmail.com>
2026-06-29 23:15:19 -0300

    :fire: feat: remoção tab-links

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Painel') }}
-        </h2>
-    </x-slot>
-
     <div x-data="{ alerta: { mostrar: false, tipo: 'success', mensagem: '' } }" @observation-saved.window="
             alerta.mostrar = true;
             alerta.tipo = 'success';
