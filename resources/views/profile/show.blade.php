3e8dfb02279d599ba59521992a9e564248a82c7f
bea <beatriztattooer@gmail.com>
2026-07-07 01:31:40 -0300

    :bug: fix: grid post

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -3,33 +3,35 @@
 
     @auth
         <div x-data="{ tab: 'feed' }" class="mt-4 max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-        <!-- Tabs -->
-        <ul class="flex border-b mb-6">
-            <li @click="tab = 'feed'"
-                :class="tab === 'feed' ? 'border-b-2 border-indigo-500' : ''"
-                class="cursor-pointer py-2 px-4">
-                Fotos
-            </li>
-            <li @click="tab = 'settings'"
-                :class="tab === 'settings' ? 'border-b-2 border-indigo-500' : ''"
-                class="cursor-pointer py-2 px-4 mr-4">
-                Configurações
-            </li>
-        </ul>
+            <!-- Tabs -->
+            <ul class="flex border-b mb-6">
+                <li @click="tab = 'feed'"
+                    :class="tab === 'feed' ? 'border-b-2 border-indigo-500' : ''"
+                    class="cursor-pointer py-2 px-4">
+                    Fotos
+                </li>
+                <li @click="tab = 'settings'"
+                    :class="tab === 'settings' ? 'border-b-2 border-indigo-500' : ''"
+                    class="cursor-pointer py-2 px-4 mr-4">
+                    Configurações
+                </li>
+            </ul>
     
-        <!-- Tab Contents -->
-        <div x-show="tab === 'feed'">
+            <!-- Tab Contents -->
+            <div x-show="tab === 'feed'">
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                    <x-profile.post :observations="$user->araucariaObservations" />
+                </div>
+            </div>
+            <div x-show="tab === 'settings'">
+                @include('profile.partials.settings')
+            </div>
+        </div>
+    @else
+        <div class="mt-4 max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                 <x-profile.post :observations="$user->araucariaObservations" />
             </div>
-        </div>
-        <div x-show="tab === 'settings'">
-            @include('profile.partials.settings')
-        </div>
-    </div>
-    @else
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            <x-profile.post :observations="$user->araucariaObservations" />
         </div>
     @endauth
 </x-app-layout>
