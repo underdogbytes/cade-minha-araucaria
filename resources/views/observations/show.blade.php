37e11f86c4cac35b929afe31d6174945bfb5c491
bea <beatriztattooer@gmail.com>
2026-07-07 00:38:28 -0300

    :zap: :recycle: refactor: isolamento de código
    - Caixa verde de dica na página de observação

--- resources/views/observations/show.blade.php
+++ resources/views/observations/show.blade.php
@@ -77,14 +77,7 @@
               </div>
             </div>
 
-            <div
-              class="bg-emerald-50 dark:bg-emerald-950/20 p-4 rounded-lg border border-emerald-100 dark:border-emerald-900 text-xs text-emerald-800 dark:text-emerald-300">
-              💡 <strong>Você sabia?</strong> As araucárias são protegidas pela
-              Lei da Mata Atlântica (Lei Federal nº 11.428/2006) e pela Lei de Crimes Ambientais (Lei Federal nº
-              9.605/1998).
-              O mapeamento colaborativo ajuda pesquisadores e a comunidade a monitorar a preservação da espécie :D
-            </div>
-
+            @include('observations.partials.do-u-know')
             <x-araucaria.report-form :observation="$observation" />
           </div>
         </div>
