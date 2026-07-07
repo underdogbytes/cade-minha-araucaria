13379e7c53b891d853c8eebb8e518c01074e92ec
bea <beatriztattooer@gmail.com>
2026-07-07 00:42:07 -0300

    :zap: :recycle: refactor: isolamento de código
    - Imagem da arvore

--- resources/views/observations/show.blade.php
+++ resources/views/observations/show.blade.php
@@ -9,14 +9,8 @@
 
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-6">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div
-            class="flex flex-col items-center justify-top bg-gray-50 dark:bg-gray-900 rounded-xl p-2 border border-gray-100 dark:border-gray-700">
-            @if($observation->photo_path)
-              <img src="{{ $observation->photo_path }}" alt="Foto da Araucária"
-                class="w-full h-auto max-h-[450px] object-cover rounded-lg shadow-md">
-            @else
-              <div class="text-gray-400 p-12 text-center">Nenhuma imagem registrada</div>
-            @endif
+          <div class="flex flex-col items-center justify-top bg-gray-50 dark:bg-gray-900 rounded-xl p-2 border border-gray-100 dark:border-gray-700">
+            @include('observations.partials.image', ['observation' => $observation])
           </div>
 
           <div class="flex flex-col justify-between space-y-6">
