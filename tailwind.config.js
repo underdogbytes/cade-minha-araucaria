7c5d5131e8a1c4a07cebf86d3f95255b72369f01
underdogbytes <underdogbytes@gmail.com>
2026-09-01 16:19:54 -0300

    :truck: build

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+        './public/js/**/*.js',
     ],
 
     theme: {
